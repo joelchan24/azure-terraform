64b38087551bbed72fec1935b6f1f54250618d3c
joel aldair chan tec <joelchan1220@gmail.com>
2021-06-10 13:19:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "azurerm_resource_group" "joel-chan" {
-name = "joel-chan"
+ name = "joel-chan"
 }
 
 resource "azurerm_virtual_network" "test-network" {
-name = "helloworld-network"
-address_space = ["10.0.0.0/16"]
-location = data.azurerm_resource_group.joel-chan.location
-resource_group_name = data.azurerm_resource_group.joel-chan.name
+  name                = "helloworld-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.joel-chan.location
+  resource_group_name = data.azurerm_resource_group.joel-chan.name
 }
